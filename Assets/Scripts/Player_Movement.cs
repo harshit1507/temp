13d5a2dd015f8cee2a98819ac27708393e26b127
harshit1507 <harshit1507@yahoo.com>
2021-07-28 23:50:10 +0530

    move enemy

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -3,66 +3,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Player_Movement : MonoBehaviour
 {
     private Touch touch;
     Rigidbody2D Rb;
-    Coroutine firingCoroutine;
     
     [SerializeField] float speed = 0.01f;
     [SerializeField] float padding = 1f;
     [SerializeField] GameObject laserPrefab;
     [SerializeField] float projectileSpeed = 10f;
-    [SerializeField] float projectileFiringPeriod = 1f;
+    [SerializeField] float projectileFiringPeriod = 0.1f;
 
     // Start is called before the first frame update
     void Start()
     {
-        Rb = GetComponent<Rigidbody2D>();        
-    }
-
-   
+        Rb = GetComponent<Rigidbody2D>();
+        StartCoroutine(FireContinously());
+    }   
 
     // Update is called once per frame
     void Update()
     {
-	Move();
+	    Move();
     }
 
     void Move()
     {
         if(Input.touchCount > 0)
         {
-
             touch = Input.GetTouch(0);
             Vector2 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
-            if(touch.phase == TouchPhase.Moved)
+            switch(touch.phase)
             {
-                Rb.MovePosition(new Vector2(touchPos.x, touchPos.y));
-		firingCoroutine = StartCoroutine(FireContinously());
-                
-            }
-	    else if(touch.phase == TouchPhase.Ended)
-	    {
-		//StopCoroutine(firingCoroutine);
-	        StopAllCoroutines();
-	    }
-
-           
+                case TouchPhase.Moved:
+                    Rb.MovePosition(new Vector2(touchPos.x, touchPos.y));                    
+                    break;
+            }           
         }
     }
-	
-    IEnumerator FireContinously()
+
+    public IEnumerator FireContinously()
     {
-	while(true)
-	{
-	    GameObject laser = Instantiate(laserPrefab, transform.position, Quaternion.identity) as GameObject;
-	    laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0f, projectileSpeed);
-	    yield return new WaitForSeconds(projectileFiringPeriod);
-	}
-    }
-    
-
-
-    
+	    while(true)
+	    {
+	        GameObject laser = Instantiate(laserPrefab, transform.position, Quaternion.identity) as GameObject;
+	        laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0f, projectileSpeed);
+	        yield return new WaitForSeconds(projectileFiringPeriod);
+	    }
+    }        
 }
