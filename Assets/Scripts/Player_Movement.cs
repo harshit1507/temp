123e1e8f77b11bec23bc3701b8fc6615aca50ba6
harshit1507 <harshit1507@yahoo.com>
2021-07-31 13:52:41 +0530

    add keyboard input for testing

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -14,10 +14,14 @@ public class Player_Movement : MonoBehaviour
     [SerializeField] GameObject laserPrefab;
     [SerializeField] float projectileSpeed = 10f;
     [SerializeField] float projectileFiringPeriod = 0.1f;
+    [SerializeField] float moveSpeed = 10f; //KeyboardInput
+
+    float xMin, xMax, yMin, yMax;   //KeyboardInput
 
     // Start is called before the first frame update
     void Start()
     {
+        SetUpMoveBoundaries();  //KeyboardInput
         Rb = GetComponent<Rigidbody2D>();
         StartCoroutine(FireContinously());
     }   
@@ -41,8 +45,31 @@ public class Player_Movement : MonoBehaviour
                     break;
             }           
         }
-    }
 
+        #region keyboardInput
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
+
+        transform.position = new Vector2(newXPos, newYPos);
+        #endregion
+    }
+    #region keyboardInputMethod
+    private void SetUpMoveBoundaries()
+    {
+        // Initialize Boundaries for the player
+        // ViewPortToWorldPoint() => Convert the position of something as it relates to the camera view into the world's space view.
+
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+    }
+    #endregion
     public IEnumerator FireContinously()
     {
 	    while(true)
