44c1828a33d79cc8ce11479a8c03cd5d82e74940
harshit1507 <harshit1507@yahoo.com>
2021-08-02 16:56:06 +0530

    stars wars v2

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,17 +15,11 @@ public class Player : MonoBehaviour
     [SerializeField] [Range(0, 1)] float deathSFXVolume = 0.75f;
     [SerializeField] AudioClip shootSound;
     [SerializeField] [Range(0, 1)] float shootSoundVolume = 0.25f;
-    //[SerializeField] float speed = 0.01f;
-    [SerializeField] float padding = 1f;    //KeyboardInput
-    [SerializeField] float moveSpeed = 10f; //KeyboardInput
 
     [Header("Projectile")]
     [SerializeField] GameObject laserPrefab;
     [SerializeField] float projectileSpeed = 10f;
     [SerializeField] float projectileFiringPeriod = 0.1f;
-
-    //GameSession gameSession;
-    float xMin, xMax, yMin, yMax;   //KeyboardInput
 
     private void Awake()
     {
@@ -47,9 +41,7 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        SetUpMoveBoundaries();  //KeyboardInput
         Rb = GetComponent<Rigidbody2D>();
-        //gameSession = FindObjectOfType<GameSession>();
         StartCoroutine(FireContinously());
     }   
 
@@ -102,32 +94,8 @@ public class Player : MonoBehaviour
             {
                  Rb.MovePosition(new Vector2(touchPos.x, touchPos.y));                    
             }           
-        }
-
-        #region keyboardInput
-        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
-        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-
-        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
-        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
-
-        transform.position = new Vector2(newXPos, newYPos);
-        #endregion
+        }   
     }
-    #region keyboardInputMethod
-    private void SetUpMoveBoundaries()
-    {
-        // Initialize Boundaries for the player
-        // ViewPortToWorldPoint() => Convert the position of something as it relates to the camera view into the world's space view.
-
-        Camera gameCamera = Camera.main;
-        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
-        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
-
-        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
-        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
-    }
-    #endregion
     public IEnumerator FireContinously()
     {
 	    while(true)
